9bcd1278045471030d4cf7404352585079f7efd1
airi <211204004+ouzuka-m@users.noreply.github.com>
2026-07-22 15:35:51 +0700

    refactor(tss): add TSS stack creation helper function

--- src/tss.rs
+++ src/tss.rs
@@ -10,25 +10,17 @@ lazy_static! {
     pub static ref TASK_STATE_SEGMENT: TaskStateSegment = {
         let mut tss = TaskStateSegment::new();
 
-        tss.interrupt_stack_table[stacks::DF_INDEX as usize] = {
-            static STACK: Stack = Stack([0u8; stacks::STACK_SIZE]);
-
-            let stack_start = VirtAddr::from_ptr(&raw const STACK);
-            stack_start + stacks::STACK_SIZE as u64
-        };
-        tss.interrupt_stack_table[stacks::NMI_INDEX as usize] = {
-            static STACK: Stack = Stack([0; stacks::STACK_SIZE]);
-
-            let stack_start = VirtAddr::from_ptr(&raw const STACK);
-            stack_start + stacks::STACK_SIZE as u64
-        };
-        tss.interrupt_stack_table[stacks::MCE_INDEX as usize] = {
-            static STACK: Stack = Stack([0; stacks::STACK_SIZE]);
-
-            let stack_start = VirtAddr::from_ptr(&raw const STACK);
-            stack_start + stacks::STACK_SIZE as u64
-        };
+        tss.interrupt_stack_table[stacks::DF_INDEX as usize] = create_stack();
+        tss.interrupt_stack_table[stacks::NMI_INDEX as usize] = create_stack();
+        tss.interrupt_stack_table[stacks::MCE_INDEX as usize] = create_stack();
 
         tss
     };
+}
+
+fn create_stack() -> VirtAddr {
+    static STACK: Stack = Stack([0; stacks::STACK_SIZE]);
+
+    let stack_start = VirtAddr::from_ptr(&raw const STACK);
+    stack_start + stacks::STACK_SIZE as u64
 }
