b0e7f24fb53c67138ef6377a16cf93188d679c79
airi <211204004+ouzuka-m@users.noreply.github.com>
2026-07-31 18:44:02 +0700

    fix: make TICKS_PER_MS thread-safe using Once

--- src/acpi/lvt/timer.rs
+++ src/acpi/lvt/timer.rs
@@ -1,5 +1,6 @@
 use core::arch::x86_64::__cpuid;
 
+use spin::once::Once;
 use x86_64::registers::model_specific::Msr;
 
 use crate::{
@@ -15,7 +16,7 @@ const CALIBRATION_MS: u64 = 10;
 
 const IA32_TSC_DEADLINE: u32 = 0x6E0;
 
-static mut TICKS_PER_MS: u64 = 0;
+static TICKS_PER_MS: Once<u64> = Once::new();
 
 pub fn init() {
     assert!(
@@ -30,14 +31,14 @@ pub fn init() {
     hpet::wait_ms(CALIBRATION_MS);
     let end = unsafe { core::arch::x86_64::_rdtsc() };
 
-    unsafe { TICKS_PER_MS = (end - start) / CALIBRATION_MS };
+    TICKS_PER_MS.call_once(|| (end - start) / CALIBRATION_MS);
 
     arm_tsc_deadline(1);
 }
 
 pub fn arm_tsc_deadline(ms: u64) {
     let now = unsafe { core::arch::x86_64::_rdtsc() };
-    let ticks_per_ms = unsafe { TICKS_PER_MS };
+    let ticks_per_ms = ticks_per_ms();
 
     let deadline = now.saturating_add(ticks_per_ms.saturating_mul(ms));
 
@@ -56,4 +57,8 @@ pub fn wrmsr(value: u64) {
 pub fn supports_tsc_deadline() -> bool {
     let cpuid = __cpuid(1);
     (cpuid.ecx & (1 << 24)) != 0
+}
+
+pub fn ticks_per_ms() -> u64 {
+    *TICKS_PER_MS.get().expect("Ticks per MS haven't calculated")
 }
